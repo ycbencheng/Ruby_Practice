790fdcbd70061d88073b14f7de5e2243b94b9b56
ycbencheng <bcyc2004@gmail.com>
2017-03-03 10:00:36 -0700

    new work

--- december/24.rb
+++ december/24.rb
@@ -1,12 +1,9 @@
 require 'rspec'
-require 'benchmark'
-
-arr = Array.new(1_000) {rand 1000}
 
 def new_max array_of_elements
   max_value = 0
 
-  arr.each do |i|
+  array_of_elements.each do |i|
     if i > max_value
       max_value = i
     end
@@ -15,19 +12,10 @@ def new_max array_of_elements
 max_value
 end
 
-Benchmark.bm(10) do |x|
-  x.report('Each: ') { max_value }
-  x.report('Sorted: ') { arr.sort.last }
+
+describe 'New Max method' do
+  it 'returns the max value from an array efficiently' do
+    test_array = [2, 4, 10, 3, 1]
+    expect(new_max(test_array)).to eq(10)
+  end
 end
-
-
-# def new_max array_of_elements
-  
-# end
-
-# describe 'New Max method' do
-#   it 'returns the max value from an array efficiently' do
-#     test_array = [2, 4, 10, 3, 1]
-#     expect(new_max(test_array)).to eq(10)
-#   end
-# end
