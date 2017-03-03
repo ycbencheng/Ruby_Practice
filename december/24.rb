f4403a504274d4c33df94f9252c8a0918491cd5d
ycbencheng <bcyc2004@gmail.com>
2017-03-03 09:27:29 -0700

    new updates

--- december/24.rb
+++ december/24.rb
@@ -1,11 +1,33 @@
 require 'rspec'
+require 'benchmark'
+
+arr = Array.new(1_000) {rand 1000}
 
 def new_max array_of_elements
+  max_value = 0
+
+  arr.each do |i|
+    if i > max_value
+      max_value = i
+    end
+  end
+
+max_value
 end
 
-describe 'New Max method' do
-  it 'returns the max value from an array efficiently' do
-    test_array = [2, 4, 10, 3, 1]
-    expect(new_max(test_array)).to eq(10)
-  end
+Benchmark.bm(10) do |x|
+  x.report('Each: ') { max_value }
+  x.report('Sorted: ') { arr.sort.last }
 end
+
+
+# def new_max array_of_elements
+  
+# end
+
+# describe 'New Max method' do
+#   it 'returns the max value from an array efficiently' do
+#     test_array = [2, 4, 10, 3, 1]
+#     expect(new_max(test_array)).to eq(10)
+#   end
+# end
--- december/29.rb
+++ december/29.rb
@@ -1,7 +1,43 @@
+# Build the Bubble Sort algorithm from scratch and add it to the self class in Ruby.
+
+# Exercise Description
+# Given an self of integers, such as:
+
+# arr = ['42', '8', '70']
+# Implement the bubble sort algorithm so that it can be called directly on an self, such as:
+
+# arr.bubble_sort
+# Sample Output
+# ['8', '42', '70']
+
+
+
 require 'rspec'
 
-describe 'Adding bubble sort to the Array class' do
-  it 'Properly sorts an array' do
+class Array
+def bubble_sort
+  n = self.length
+  
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if self[i] > self[i +1]
+        self[i], self[i+1] = self[i +1], self[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+
+  self
+end
+end
+
+
+
+describe 'Adding bubble sort to the self class' do
+  it 'Properly sorts an self' do
     arr = [4, 1, 6, 10, 44, 2, 3]
     expect(arr.bubble_sort).to eq(arr.sort)
   end
