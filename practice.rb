1ac15737f7eafec550ba86dbe36cfa5b3697c757
ycbencheng <bcyc2004@gmail.com>
2017-03-08 18:09:45 -0700

    Built List name, items, and amount

--- practice.rb
+++ practice.rb
@@ -1,5 +1,28 @@
-grocery_list = ["milk", "eggs", "bread"]
-grocery_list << "carrots"
-grocery_list.push("pizza")
-grocery_list.unshift("juice", "apple")
-puts grocery_list.inspect
+
+#building a grocery list
+def create_list
+  print "What is the list name?"
+  name = gets.chomp
+
+  hash = { "name" => name, "items" => Array.new}
+  return hash
+end
+
+def add_list_item
+  print "What is the item called?"
+  item_name = gets.chomp
+
+  print "How many?"
+  quantity = gets.chomp.to_i
+
+  hash = {"name" => item_name, "quantity" => quantity}
+  return hash
+end
+
+
+
+list = create_list()
+puts list.inspect
+list['items'].push(add_list_item())
+
+puts list.inspect
