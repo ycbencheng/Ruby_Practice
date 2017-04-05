84477fad80666c6c17934d55527c5d8c889673bb
ycbencheng <bcyc2004@gmail.com>
2017-04-05 10:39:37 -0600

    cleaned old files

--- january/8.rb
+++ january/8.rb
@@ -1,7 +1,10 @@
 require 'rspec'
+require 'bigdecimal'
 
 def large_number_processor num_1, num_2
+  (BigDecimal(num_1) * BigDecimal(num_2)).to_s('F')cd .. 
 end
+
 
 describe 'Large number processing' do
   it 'accurately multiplies 2 large numbers' do
---
+++ piglatin.rb
@@ -0,0 +1,14 @@
+# "The quick brown fox" becomes "Hetay uickqay rownbay oxfay"
+# 1. Put the first letter of each word at the end
+# 2. Add "ay" after
+# 3. If a word has one sylable with a vowel, only ay is added 
+
+  puts "Which phrase would you want to translate?"
+  answer = gets.chomp
+  
+  new_words = answer.split(//)
+  puts new_words
+
+
+
+
